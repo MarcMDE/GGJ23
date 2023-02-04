fcb69ff859eb1bc2e7bf38851642ef72678c1fb4
marcmde <marcmde95@gmail.com>
2023-02-04 19:18:00 +0100

    Gather message

--- UnityProject/Assets/_Core_/Scripts/PlayerController.cs
+++ UnityProject/Assets/_Core_/Scripts/PlayerController.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 { 
     const int _maxCollectables = 10;
     int _nCollectables;
     CollectableTypes _collectableGathered;
+    CollectableBehaviour _currentCollidingCollectable;
+
+    public UnityAction OnCollectableRangeEnter;
+    public UnityAction OnCollectableRangeExit;
 
     private void Start()
     {
         _nCollectables = 0;
         _collectableGathered = CollectableTypes.NONE;
+        _currentCollidingCollectable = null;
     }
 
     public void ResetGatheredCollectables()
@@ -34,13 +40,41 @@ public class PlayerController : MonoBehaviour
             _nCollectables = _maxCollectables;
     }
 
+    public bool IsInGatheringRange()
+    {
+        return _currentCollidingCollectable is not null;
+    }
+
+    private void Update()
+    {
+        if (_currentCollidingCollectable != null && Input.GetKey(KeyCode.E))
+        {
+            // Gather collectable
+            GatherCollectable(_currentCollidingCollectable.Type);
+            _currentCollidingCollectable.Gather();
+
+            OnCollectableRangeExit.Invoke();
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         CollectableBehaviour c = other.GetComponent<CollectableBehaviour>();
         // Show message
         // enable gathering
-        GatherCollectable(c.Type);
-        c.Gather();
+        _currentCollidingCollectable = c;
+        OnCollectableRangeEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CollectableBehaviour c = other.GetComponent<CollectableBehaviour>();
+
+        if (c == _currentCollidingCollectable)
+        {
+            c = null;
+            OnCollectableRangeExit.Invoke();
+        }
     }
 
 }

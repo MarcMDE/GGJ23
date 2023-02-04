6c65f477748a6de15b903e19843b5e1fc5febec1
marcmde <marcmde95@gmail.com>
2023-02-04 21:13:18 +0100

    Roots coloring unity example

--- UnityProject/Assets/_Core_/Scripts/ColorRoots.cs
+++ UnityProject/Assets/_Core_/Scripts/ColorRoots.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Environments { AUTUM = 0, WINTER, SPRING, NONE}
 
 public class ColorRoots : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const int _nEnvironments = 4;
+    Color[] _colors = new Color[_nEnvironments];
+
+    [SerializeField]
+    GameObject _object;
+
     void Start()
     {
-        
+        _colors[(int)Environments.AUTUM] = Color.red;
+        _colors[(int)Environments.WINTER] = Color.white;
+        _colors[(int)Environments.SPRING] = Color.green;
+        _colors[(int)Environments.NONE] = Color.black;
+
+        Mesh mesh = _object.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        // create new colors array where the colors will be created.
+        Color[] colors = new Color[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+            colors[i] = Color.Lerp(Color.red, Color.green, vertices[i].y);
+
+        // assign the array of colors to the Mesh.
+        mesh.colors = colors;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
